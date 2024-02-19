5c578b30e2c23eb66ec8968b93462a067698b4b4
huylvph30524 <vhuy887@gmail.com>
2024-02-20 00:58:43 +0700

    huy update phong

--- screens/RoomDetail.js
+++ screens/RoomDetail.js
@@ -1,15 +1,23 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity, Modal, TextInput } from "react-native";
-import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Modal,
+  TextInput,
+} from "react-native";
+import React, { useState, useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation, useIsFocused } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/Ionicons";
 import * as ImagePicker from "expo-image-picker";
-import { collection, addDoc, getDocs, getDoc, doc, updateDoc} from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
+import { collection, addDoc, getDocs, getDoc, doc, updateDoc} from "firebase/firestore";
 import { app } from "./firebaseConfig";
 
 const RoomDetail = ({ route }) => {
-  const { getRoom } = route.params;
+  const [getRoom, setgetRoom] = useState(route.params.getRoom);
   const { userId } = route.params;
   console.log(userId);
   const [nguoithue, setnguoithue] = useState({
@@ -18,22 +26,29 @@ const RoomDetail = ({ route }) => {
     gender: "",
     image: "",
     roomid: getRoom.roomid,
-    userid: userId
+    userid: userId,
   });
   const defaultImage = require("../assets/Group.png");
   const navigation = useNavigation();
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const [isModalVisibles, setIsModalVisibles] = useState(false);
+  const [isUpdate, setIsUpdate] = useState(false);
 
+  const isModalUpdate = () => {
+    setIsUpdate(!isUpdate);
+    delAnh();
+  };
   const toggleModals = () => {
     setIsModalVisibles(!isModalVisibles);
+    setIsModalVisible(false);
     setnguoithue({
       name: "",
       phone: "",
       gender: "",
       image: "",
       roomid: getRoom.roomid,
-      userid: userId
-    })
+      userid: userId,
+    });
   };
   const handleBackPress = () => {
     navigation.goBack();
@@ -45,7 +60,7 @@ const RoomDetail = ({ route }) => {
       price = price.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     }
     return price;
-  };
+  }
 
   const pickImageUpdate = async () => {
     // No permissions request is necessary for launching the image library
@@ -56,7 +71,7 @@ const RoomDetail = ({ route }) => {
       quality: 1,
     });
     if (!result.canceled) {
-      setgetRoom({...getRoom ,anhphong: result.assets[0].uri});
+      setgetRoom({ ...getRoom, anhphong: result.assets[0].uri });
       //console.log('base64: '+result.uri.base64);
       setImage(result.assets[0].uri);
     }
@@ -88,28 +103,22 @@ const RoomDetail = ({ route }) => {
     console.log("Vào hàm update");
     const firestore = getFirestore(app);
     try {
-        await updateDoc(doc(firestore, "rooms", getRoom.roomid), {
-            tenphong: getRoom.tenphong,
-            giaphong: getRoom.giaphong,
-            dientich: getRoom.dientich,
-            mota: getRoom.mota,
-            soluongtoida: getRoom.soluongtoida,
-            anhphong: getRoom.anhphong,
-        });
-        console.log("Rooms data updated successfully!");
+      await updateDoc(doc(firestore, "rooms", getRoom.roomid), {
+        tenphong: getRoom.tenphong,
+        giaphong: getRoom.giaphong,
+        dientich: getRoom.dientich,
+        mota: getRoom.mota,
+        soluongtoida: getRoom.soluongtoida,
+        anhphong: getRoom.anhphong,
+      });
+      console.log("Rooms data updated successfully!");
     } catch (error) {
-        console.error("Error updating Rooms data: ", error);
+      console.error("Error updating Rooms data: ", error);
     }
-};
+  };
 
-  const handletenPhong = (value) => {
-    setgetRoom({ ...getRoom, tenphong: value });
-  };
-  const handledienTich = (value) => {
-    setgetRoom({ ...getRoom, dientich: value });
-  };
-  const handlegiaPhong = (value) => {
-    setgetRoom({ ...getRoom, giaphong: value });
+  const handleNameOnChange = (value) => {
+    setnguoithue({ ...nguoithue, name: value });
   };
   const handlePhoneOnChange = (value) => {
     setnguoithue({ ...nguoithue, phone: value });
@@ -121,6 +130,33 @@ const RoomDetail = ({ route }) => {
     setnguoithue({ ...nguoithue, image: value });
   };
 
+  const handletenPhong = (value) => {
+    setgetRoom({ ...getRoom, tenphong: value });
+  };
+  const handledienTich = (value) => {
+    setgetRoom({ ...getRoom, dientich: value });
+  };
+  const handlegiaPhong = (value) => {
+    setgetRoom({ ...getRoom, giaphong: value });
+  };
+  const handlemoTa = (value) => {
+    setgetRoom({ ...getRoom, mota: value });
+  };
+  const handlesoluongMax = (value) => {
+    setgetRoom({ ...getRoom, soluongtoida: value });
+  };
+  const handleanhPhong = (value) => {
+    setgetRoom({ ...getRoom, anhphong: getRoom.anhphong });
+  };
+  const delAnh = () => {
+    setgetRoom({ ...getRoom, anhphong: getRoom.anhphong });
+  };
+  const UpdateRoom = () => {
+    updateRoomData(getRoom);
+    console.log(getRoom);
+    isModalUpdate();
+  };
+
   const [thanhvien, setThanhVien] = useState(getRoom.thanhvien);
   const addNguoiThue = async () => {
     const db = getFirestore(app);
@@ -128,36 +164,37 @@ const RoomDetail = ({ route }) => {
       const docRef = await addDoc(collection(db, "nguoithuephongs"), nguoithue);
       thanhvien.push(docRef.id);
       console.log("Thêm người thuê thành công: ", docRef.id);
-      updateRoomField(getRoom.roomid, "thanhvien", thanhvien)
+      updateRoomField(getRoom.roomid, "thanhvien", thanhvien);
 
       setIsModalVisibles(false);
     } catch (e) {
       console.error("Error adding document: ", e);
     }
-  }
+  };
+
   const updateRoomField = async (roomId, fieldToUpdate, updatedValue) => {
     const db = getFirestore(app);
     const docRef = doc(db, "rooms", roomId); // Xác định vị trí của tài liệu
     try {
       await updateDoc(docRef, {
-        [fieldToUpdate]: updatedValue // Cập nhật trường fieldToUpdate với giá trị updatedValue
+        [fieldToUpdate]: updatedValue, // Cập nhật trường fieldToUpdate với giá trị updatedValue
       });
       console.log("Document successfully updated!");
     } catch (e) {
       console.error("Error updating document: ", e);
     }
-  }
-  
-  
-  
+  };
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <TouchableOpacity onPress={handleBackPress} style={styles.backButton}>
           <Icon name="arrow-back" size={24} color="black" />
         </TouchableOpacity>
-        <TextInput style={styles.title} value={getRoom.tenphong}/>
-        <TouchableOpacity style={{marginLeft:'auto'}} onPress={isModalUpdate}>
+        <TextInput style={styles.title} value={getRoom.tenphong} />
+        <TouchableOpacity
+          style={{ marginLeft: "auto" }}
+          onPress={isModalUpdate}
+        >
           <Icon name="sync" size={24} color="black" />
         </TouchableOpacity>
       </View>
@@ -192,31 +229,34 @@ const RoomDetail = ({ route }) => {
           <Text style={styles.overlayNumber}>{getRoom.datcoc}</Text>
         </View>
       </View>
-      <TouchableOpacity style={{
-        width: "50%",
-        backgroundColor: "#33CCFF",
-        padding: 12,
-        borderRadius: 40,
-        marginTop: 130,
-        borderWidth: 2,
-        marginLeft: 120
-      }} onPress={toggleModals}>
-        <Text style={{
-          textAlign: 'center', fontWeight: 'bold',
-          color: 'white'
-        }} >Thêm người thuê</Text>
-      </TouchableOpacity>
-
-      <Modal
-        animationType="fade"
-        transparent={true}
-        visible={isModalVisibles}
+      <TouchableOpacity
+        style={{
+          width: "50%",
+          backgroundColor: "#33CCFF",
+          padding: 12,
+          borderRadius: 40,
+          marginTop: 130,
+          borderWidth: 2,
+          marginLeft: 120,
+        }}
+        onPress={() => toggleModals(true)}
       >
+        <Text
+          style={{ textAlign: "center", fontWeight: "bold", color: "white" }}
+        >
+          Thêm người thuê
+        </Text>
+      </TouchableOpacity>
+      <Modal animationType="fade" transparent={true} visible={isModalVisibles}>
         <View style={styles.modalContainer}>
           <View style={styles.modalContents}>
             <TouchableOpacity onPress={pickImageUser} style={{ marginBottom: 30 }}>
               <Image
-                source={nguoithue && nguoithue.image != "" ? { uri: nguoithue.image } : defaultImage}
+                source={
+                  nguoithue && nguoithue.image != ""
+                    ? { uri: nguoithue.image }
+                    : defaultImage
+                }
                 style={styles.avatar}
               />
             </TouchableOpacity>
@@ -229,11 +269,10 @@ const RoomDetail = ({ route }) => {
                 marginBottom: 12,
                 borderWidth: 2,
                 textAlign: "center",
-              }} onChangeText={handleNameOnChange}
+              }}
+              onChangeText={handleNameOnChange}
+            ></TextInput>
 
-            >
-
-            </TextInput>
             <TextInput
               style={{
                 width: "100%",
@@ -245,9 +284,7 @@ const RoomDetail = ({ route }) => {
                 textAlign: "center",
               }}
               onChangeText={handlePhoneOnChange}
-
-            >
-            </TextInput>
+            ></TextInput>
             <TextInput
               style={{
                 width: "100%",
@@ -258,10 +295,8 @@ const RoomDetail = ({ route }) => {
                 borderWidth: 2,
                 textAlign: "center",
               }}
-
               onChangeText={handleGenderOnChange}
-            >
-            </TextInput>
+            ></TextInput>
             <View
               style={{
                 flexDirection: "row",
@@ -286,6 +321,68 @@ const RoomDetail = ({ route }) => {
           </View>
         </View>
       </Modal>
+      {/* Dialog sửa phòng */}
+      <Modal visible={isUpdate} animationType="slide">
+        <View style={styles.modalContainerz}>
+          <Text style={styles.modalTitle}>Sửa Phòng</Text>
+          <TouchableOpacity onPress={pickImageUpdate}>
+            <Image
+              source={
+                getRoom.anhphong ? { uri: getRoom.anhphong } : defaultImage
+              }
+              style={{
+                width: 180,
+                height: 130,
+                borderRadius: 5,
+                borderWidth: 2,
+                borderColor: "blue",
+              }}
+            />
+          </TouchableOpacity>
+          <TextInput
+            style={styles.input}
+            placeholder="Tên phòng"
+            value={getRoom.tenphong}
+            onChangeText={handletenPhong}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Giá phòng"
+            value={getRoom.giaphong}
+            onChangeText={handlegiaPhong}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Diện tích"
+            value={getRoom.dientich}
+            onChangeText={handledienTich}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Mô tả"
+            value={getRoom.mota}
+            onChangeText={handlemoTa}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Số lượng tối đa"
+            value={getRoom.soluongtoida}
+            keyboardType="numeric"
+            onChangeText={handlesoluongMax}
+          />
+          <View style={styles.modalButtons}>
+            <TouchableOpacity style={styles.modalButton} onPress={UpdateRoom}>
+              <Text style={styles.modalButtonText}>Lưu</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.modalButton}
+              onPress={isModalUpdate}
+            >
+              <Text style={styles.modalButtonText}>Hủy</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 };
@@ -306,6 +403,10 @@ const styles = StyleSheet.create({
   },
   backButton: {
     marginRight: 10,
+  },
+  refButton: {
+    marginRight: 10,
+    justifyContent: "space-evenly",
   },
   title: {
     fontSize: 20,
@@ -362,6 +463,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "rgba(0, 0, 0, 0.5)", // opacity for the background
   },
+  modalContainerz: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   modalContents: {
     backgroundColor: "#FFF",
     padding: 20,
@@ -392,5 +498,35 @@ const styles = StyleSheet.create({
     marginLeft: 115,
     borderColor: "green",
     borderWidth: 5,
+  },
+  input: {
+    width: "80%",
+    height: 40,
+    marginVertical: 10,
+    padding: 10,
+    paddingLeft: 30,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  modalButtons: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  modalButton: {
+    backgroundColor: "#10DEDE", // Màu nền của nút
+    padding: 10, // Khoảng cách giữa nút và kích thước nút
+    borderRadius: 8, // Bo góc của nút
+    margin: 10,
+  },
+  modalButtonText: {
+    color: "white", // Màu chữ của nút
+    textAlign: "center", // Căn giữa nội dung của nút
+    fontWeight: "bold",
   },
 });
